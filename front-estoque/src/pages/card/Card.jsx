7ade61e0531fd4bee8bbb1c916f7abdcbf36274a
Eduardo Ennes <eduardosimoes9899@gmail.com>
2025-04-22 22:41:17 -0300

    termino da lista do admin e nao logado e header admin

--- front-estoque/src/pages/card/Card.jsx
+++ front-estoque/src/pages/card/Card.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import '../../app.css'
 
 function Card() {
+
+  const [Card, setCard] = useState([])
+
   return (
-    <div>Card</div>
+    <>
+      {Card.length > 0 ? (
+        <div className='div-card'>
+          <div className='div-card-contein'>
+            <p className='div-card-contein-p-name'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, vero!</p>
+            <div className='div-card-contein-spanqtd-and-price'>
+              <span className='span-card-qtd'><Link className='Link-span-qtd'>-</Link><p>10</p><Link className='Link-span-qtd'>+</Link></span>
+              <p className='div-card-contein-price'>R$250.000,00</p>
+            </div>
+          </div>
+        </div>
+      ):(
+        <div className='div-card'>
+          <h1 className='H1-title-card-payments-empty'>Carrinho vazio!</h1>
+        </div>
+      )}
+      
+      <div className='div-card-payments'>
+        <span className='div-card-span-payments'>
+          <p>Total:</p>
+          <p>R$1.000,00</p>
+        </span>
+        <Link className='Link-card-payments'>Fechar Pedido</Link>
+      </div>
+    </>
   )
 }
 
--- front-estoque/src/pages/list/List.jsx
+++ front-estoque/src/pages/list/List.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../app.css'
 
 function List({Permission, onSelectId}) {
 
   console.log('Permission List - ', Permission)
+  const [Products, setProducts] = useState([1])
 
   const handleClick = (id) => {
     onSelectId(id); // Passa o ID para o App
@@ -11,63 +12,116 @@ function List({Permission, onSelectId}) {
 
   return (
     <>
-      {Permission ? 
-        <button onClick={() => handleClick(51)}>Editar</button>
-      :
-        <>
-          <nav className='nav-search'>
-            <select name="select" className='select'>
-              <option value="1">Tecnologia</option>
-              <option value="2">Ferramenta</option>
-              <option value="3">Roupa</option>
-              <option value="4">Livro</option>
-              <option value="5">Parafuso</option>
-            </select>
-            <div className='div-search-position'>
-              <input type="text" className='search' placeholder='Buscar produto'/>
-              <button className='button_icon-search'><span className="material-icons">search</span></button>
+      <nav className='nav-search'>
+        <select name="select" className='select'>
+          <option value="1">Tecnologia</option>
+          <option value="2">Ferramenta</option>
+          <option value="3">Roupa</option>
+          <option value="4">Livro</option>
+          <option value="5">Parafuso</option>
+        </select>
+        <div className='div-search-position'>
+          <input type="text" className='search' placeholder='Buscar produto'/>
+          <button className='button_icon-search'><span className="material-icons">search</span></button>
+        </div>
+      </nav>
+
+      
+        {Permission ? 
+          <>
+            <div className='div-table-position-fixed'>
+              {Products.length > 0 ? (
+                <table className='table'>
+                  <thead className='thead-list-fixed'>
+                    <tr className='tr-table'>
+                      <th className='list-th-size-name'>Nome do produto</th>
+                      <th>preço</th>
+                      <th>Promoção</th>
+                      <th>Preço.P</th>
+                      <th>Qtd</th>
+                      <th>Ação</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    <tr className='tr-table'>
+                      <td title='hahahahahhahhaha' className='list-td-name-product'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, vero!</td>
+                      <td>R$200,00</td>
+                      <td>Sim</td>
+                      <td>R$180,00</td>
+                      <td>20</td>
+                      <td className='td-position-button'>
+                        <button className='button-add-card'>Carrinho</button>
+                        <button>&#10060;</button>
+                        <button>r</button>
+                      </td>
+                    </tr>
+
+                    <tr className='tr-table'>
+                      <td title='hahahahahhahhaha' className='list-td-name-product'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, vero!</td>
+                      <td>R$200,00</td>
+                      <td>Sim</td>
+                      <td>R$180,00</td>
+                      <td>20</td>
+                      <td className='td-position-button'>
+                        <button className='button-add-card'>Carrinho</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              ):(
+                <h1 className='H1-title-list-empty'>Você não possui produtos em estoque ou cadastrados!</h1>
+              )}
             </div>
-          </nav>
+          </>
+        :
+          <>
+          <div className='div-table-position-fixed-false'>
+            {Products.length > 0 ? ( 
+              <table className='table'>
+                <thead className='thead-list-fixed'>
+                  <tr className='tr-table'>
+                    <th className='list-th-size-name'>Nome do produto</th>
+                    <th>preço</th>
+                    <th>Promoção</th>
+                    <th>Preço.P</th>
+                    <th>Qtd</th>
+                    <th>Ação</th>
+                  </tr>
+                </thead>
 
-          <div className='div-table-position-fixed'>
-            <table className='table'>
-              <thead>
-                <tr className='tr-table'>
-                  <th>Nome</th>
-                  <th>preço</th>
-                  <th>Promoção</th>
-                  <th>Preço.P</th>
-                  <th>Quantidade</th>
-                  <th>Ação</th>
-                </tr>
-              </thead>
+                <tbody>
+                  <tr className='tr-table'>
+                    <td title='hahahahahhahhaha' className='list-td-name-product'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, vero!</td>
+                    <td>R$200,00</td>
+                    <td>Sim</td>
+                    <td>R$180,00</td>
+                    <td>20</td>
+                    <td className='td-position-button'>
+                      <button className='button-add-card'>Carrinho</button>
+                    </td>
+                  </tr>
 
-              <tbody>
-                <tr className='tr-table'>
-                  <td title='hahahahahhahhaha'>Camisa Fluminense</td>
-                  <td>R$200,00</td>
-                  <td>Sim</td>
-                  <td>R$180,00</td>
-                  <td>20</td>
-                  <td className='td-position-button'>
-                    <button className='button-add-card'>Carrinho</button>
-                  </td>
-                </tr>
-                <tr className='tr-table'>
-                  <td title='ha'>Camisa Fluminense</td>
-                  <td>R$200,00</td>
-                  <td>Sim</td>
-                  <td>R$180,00</td>
-                  <td>20</td>
-                  <td className='td-position-button'>
-                    <button className='button-add-card'>Carrinho</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </>
-      }
+                  <tr className='tr-table'>
+                    <td title='ha' className='list-td-name-product'>Camisa Fluminense</td>
+                    <td>R$2.000,00</td>
+                    <td>Sim</td>
+                    <td>R$180,00</td>
+                    <td>20</td>
+                    <td className='td-position-button'>
+                      <button className='button-add-card'>Carrinho</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              
+              ):(
+                <h1 className='H1-title-list-empty'>Você não possui produtos em estoque ou cadastrados!</h1>
+              )}
+            </div>
+          </>
+        }
+        
     </>
   )
 }
